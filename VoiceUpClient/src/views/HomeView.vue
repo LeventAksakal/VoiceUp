54a4739f42c32d4b49b17fa96216236dfa90ed85
Gurol-Onat-Ulutas <128299276+Gurol-Onat-Ulutas@users.noreply.github.com>
2024-01-09 17:32:15 +0300

    Style added to the project

--- VoiceUpClient/src/views/HomeView.vue
+++ VoiceUpClient/src/views/HomeView.vue
@@ -7,10 +7,57 @@ const requestVideoChat = () => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold mb-8">VoiceUp</h1>
-    <button @click="requestVideoChat" class="px-4 py-2 bg-blue-500 text-white rounded">
-      Request Video Chat
-    </button>
-  </div>
+  <main>
+    <div>
+      <h1>VoiceUp!</h1>
+      <button @click="requestVideoChat">
+        Request Video Chat
+      </button>
+    </div>
+
+
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color:  #3d348b;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+
+}
+
+h1 {
+  color:  #f7b801;
+  font-size: 50px;
+  margin-bottom: 25px;
+
+}
+
+
+button{
+  background: #f18701;
+  border-radius: 999px;
+  box-sizing: border-box;
+  color: rgb(230, 232, 230);
+  cursor: pointer;
+  font-family: Inter,Helvetica,"Apple Color Emoji","Segoe UI Emoji",NotoColorEmoji,"Noto Color Emoji","Segoe UI Symbol","Android Emoji",EmojiSymbols,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue","Noto Sans",sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  opacity: 1;
+  outline: 0 solid transparent;
+  padding: 8px 18px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: fit-content;
+  word-break: break-word;
+  border: 0;
+}
+</style>
+
--- VoiceUpClient/vite.config.js
+++ VoiceUpClient/vite.config.js
@@ -6,9 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  css: {
-    postcss: './postcss.config.cjs'
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
